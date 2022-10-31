d0936048593650b29cec493e872fc55713599c5c
JacksonRathgeber <jr5348@nyu.edu>
2022-10-31 14:07:29 -0400

    Second push (critique)

--- Midterm-Project/classes.js
+++ Midterm-Project/classes.js
@@ -1,34 +1,34 @@
 class Core {
 
 	constructor(r_, g_, b_){
-		this.x=width/2;
-		this.y=height/2;
+		this.pos=createVector(width/2,height/2);
 		this.color=color(r_,g_,b_);
 		this.size=80;
 	}
 
-	display(){
+	display(){ //create image of core
 		fill(this.color);
-		ellipse(this.x,this.y,this.size,this.size);
+		ellipse(this.pos.x,this.pos.y,this.size,this.size);
 	}
 
-	changeHue(xr,xg,xb){
+	changeHue(xr,xg,xb){ //change color
 		this.color=color(xr,xg,xb);
 	}
 
-	vibrate(intensity){
-		this.x=width/2+random(-intensity,intensity);
-		this.y=height/2+random(-intensity,intensity);
+	vibrate(intensity){ // vibrate w random movements
+		this.pos.x=width/2+random(-intensity,intensity);
+		this.pos.y=height/2+random(-intensity,intensity);
 	}
 	
-	recenter(){
-		this.x=width/2;
-		this.y=height/2;
+	recenter(){ //return core to center of screen
+		this.pos.x=width/2;
+		this.pos.y=height/2;
 	}
 
-	changeSize(newSize){
+	changeSize(newSize){ //augment size
 		this.size=newSize;
 	}
+
 }
 
 
@@ -37,21 +37,81 @@ class Core {
 class Projectile{
 
 	constructor(speed){
-		this.pos=createVector(0,height/2);
+		this.pos=createVector(-60,height/2);
 		this.speed=createVector(speed,0);
 		this.opacity=255;
+		this.size=40;
 	}
 
-	display(){
+	display(){ //create image of projectile
+		noStroke();
 		fill(150,0,0,this.opacity);
-		ellipse(this.pos.x,this.pos.y,40,40)
+		ellipse(this.pos.x,this.pos.y,this.size,this.size);
 	}
 
-	fire(){
+	fire(){ //shoot projectile
 		this.pos.add(this.speed);
+	}
+
+	destroy(){ //projectile disappears
+		this.opacity=0;
+	}
+
+	intersect(core){ //check intersection with core
+		if (core.pos.dist(this.pos)<=this.size){
+		  return true;
+		}
+	return false;
+	}
+
+	retreat(){ //move away from core
+		this.pos.sub(this.speed.x/2);
+	}
+
+	changeSpeed(newSpeed){ //augment speed
+		this.speed=newSpeed;
 	}
 }
 
+class BackgroundLine{
+	constructor(x_){
+		this.x=x_;
+		this.toppos=createVector(this.x,0);
+		this.bottompos=createVector(this.x,height);
+		this.topvel=createVector(random(-40,40),random(80,-5));
+    	this.bottomvel=createVector(random(-40,40),random(80,-5));
+		this.grav=createVector(0,3);
+		this.finTime=0;
+		this.frames=0;
+	}
 
+	display(timer){
+		let opac=map(this.frames,0,timer*60,0,120);
+		if(this.finTime<=timer){
+			stroke(random(100,255),0,0,opac);
+			line(this.toppos.x,this.toppos.y,this.bottompos.x,this.bottompos.y);
+			console.log("opac: "+opac);
+		}
+		else{
+			line(this.toppos.x,this.toppos.y,this.bottompos.x,this.bottompos.y);
+		}
+	}
+	//lines fade in, random stroke colors
 
+	count(){ //independent time keeper for end sequence
+ 	 	if(frameCount%60==0){
+    		this.finTime++;
+		}
+		this.frames++;
+	}
 
+	collapse(timer){
+		if(this.finTime>=timer){
+			this.toppos.add(this.topvel);
+			this.bottompos.add(this.bottomvel);
+			this.topvel.add(this.grav);
+			this.bottomvel.add(this.grav);
+		}
+	}
+	//lines fall offscreen
+}
--- Midterm-Project/sketch.js
+++ Midterm-Project/sketch.js
@@ -1,42 +1,149 @@
-let phase=0;
 let sec=0;
-let tick=false;
 let core;
-let proj1;
+let projs=[];
+let lines=[];
+let coreColor;
+let finishFlag=false;
 
 function setup() {
   createCanvas(800,800);
-  core=new Core(160,210,250);
-  proj1=new Projectile(20);
+  
+  coreColor=color(160,210,250);
+  core=new Core(coreColor); // core = "main character"
+  
+  projs[0]=new Projectile(1);
+  projs[1]=new Projectile(1.5); //projectiles = negativity? something like that
+  projs[2]=new Projectile(5);
+  
+  for (let i=3;i<=6;i++){
+    projs[i]=new Projectile(10); //set up "rapid-fire" near end
+    projs[i].pos.x-=80*(i-2);
+    //console.log(i+" location: "+projs[i].pos.x);
+  }
+
+  for (let i=0;i<=width;i++){
+    lines[i]=new BackgroundLine(i);
+    //set up crazy background, create objects for each line
+  }
 }
 
 function draw() {
 	background(0);
+  
+  core.display(); //show core
+
+  if(sec==0){
+    core.changeHue(160,210,250);
+    finishFlag=false;
+    core.vibrate(0);
+
+    projs=[];
+    lines=[];
+
+    projs[0]=new Projectile(1);
+    projs[1]=new Projectile(1.5);
+    projs[2]=new Projectile(5);
+  
+    for (let i=3;i<=6;i++){
+      projs[i]=new Projectile(10);
+      projs[i].pos.x-=80*(i-2);
+      //console.log(i+" location: "+projs[i].pos.x);
+    }
+    for (let i=0;i<=width;i++){
+      lines[i]=new BackgroundLine(i);
+      lines[i].topvel=createVector(random(-40,40),random(40,-5));
+      lines[i].bottomvel=createVector(random(-40,40),random(40,-5));
+    }
+    //reset sketch after certain time elapsed, create loop
+  }
+
   if (frameCount%60==0){
   	sec++;
-  	tick=true;
+    console.log(sec);
   }
-  else{
-  	tick=false;
-  }
-
-  core.display();
-
   
-  if (sec==2){
-  	core.changeHue(200,0,0);
-  	core.vibrate(10);
+  if (sec==34){
+    sec=0;
   }
-  else{
-  	core.recenter();
+  //looping counter
+
+  if(sec>=4 && sec<12){
+    projs[0].display();
+    projs[0].fire();
+    if (projs[0].intersect(core)){
+      projs[0].destroy();
+      core.changeHue(190,170,200);
+    }
+  }
+  //launch first projectile, disappear on collision with core
+
+  if(sec>=15 && sec<20){
+    projs[1].display();
+    projs[1].fire();
+    if (projs[1].intersect(core)){
+      projs[1].destroy();
+      core.changeHue(210,150,190);
+    }
+  }
+  //launch second projectile, functionally identical to first
+
+
+  if(sec>=24 && sec<=29){
+    projs[2].display();
   }
 
-  if (sec==4){
-  	proj1.display();
-  	proj1.fire();
+  if(sec>=24 && sec<25){
+    projs[2].fire();    
   }
-}
+  if(sec>=25 && sec<=27){
+    projs[2].retreat();
+  }
+  if (sec==25){
+    core.changeHue(240,120,150);
+    core.pos.sub(1,0);
+    core.changeSize(100);
+  }
 
-function intersect(c = new Core(r_,g_,b_), p = new Projectile(speed)){
+  if (sec==26){
+    core.changeHue(210,150,190);
+    core.recenter();
+    core.changeSize(80);
+    core.vibrate(2);
+  }
 
+  if (sec>=27 && sec<29){
+    core.vibrate(2);
+  }
+
+  if (sec>=29){
+    core.vibrate(2);
+
+  //third projectile approaches, is "scared off", retreats
+    
+    for (let i=2;i<=6;i++){
+      projs[i].display();
+      projs[i].changeSpeed(10);
+      projs[i].fire();
+      if(projs[i].intersect(core)){
+        projs[i].destroy();
+        core.vibrate(5);
+      }
+    }
+    if (projs[6].intersect(core)){
+      finishFlag=true;
+    }
+  }
+  //third projectile returns with rapid-fire, triggers end sequence
+  
+  if (finishFlag==true){
+    core.changeHue(255,0,0);
+    core.vibrate(10);
+
+    for (let i=0;i<=width;i++){
+      lines[i].count();
+      lines[i].display(3);
+      lines[i].collapse(3);
+    }
+  }
+  //background lines appear, fall off, core vibrates violently and turns completely red
 }
